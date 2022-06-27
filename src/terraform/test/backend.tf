dc43a4ca4dfb8d0ed3f98ea652416eb300d11c93
Neh Lata <neh.lata@cloudeq.com>
2022-06-27 17:13:25 +0530

    backend updated

--- src/terraform/test/backend.tf
+++ src/terraform/test/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-      bucket = "s3-for-assignment"
-      key="terraform.tfstate"
-      dynamodb_table = "NehLata-table"
-      region = "us-east-1"
+      # bucket = "s3-for-assignment"
+      # key="terraform.tfstate"
+      # dynamodb_table = "NehLata-table"
+      # region = "us-east-1"
   
     
   }
