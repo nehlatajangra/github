d6d8beecd6dc91f05b10301cf52eae07265571eb
Neh Lata <neh.lata@cloudeq.com>
2022-06-27 14:35:46 +0530

    file added

--- src/terraform/module/main.tf
+++ src/terraform/module/main.tf
@@ -7,6 +7,42 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "${var.subnet_cidr}"
+  availability_zone = "${var.availability_zone}"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+# resource "aws_internet_gateway" "my_vpc_igw" {
+#   vpc_id = aws_vpc.my_vpc.id
+
+#   tags = {
+#     Name = "My VPC - Internet Gateway"
+#   }
+# }
+
+# resource "aws_route_table" "my_vpc_us_east_1a_public" {
+#     vpc_id = aws_vpc.my_vpc.id
+
+#     route {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.my_vpc_igw.id
+#     }
+
+#     tags = {
+#         Name = "Public Subnet Route Table."
+#     }
+# }
+
+# resource "aws_route_table_association" "my_vpc_us_east_1a_public" {
+#     subnet_id = aws_subnet.public.id
+#     route_table_id = aws_route_table.my_vpc_us_east_1a_public.id
+# }
+
 resource "aws_security_group" "sg" {
   name        = "allow_sg"
   description = "Allow SSH inbound connections"
---
+++ src/terraform/test/main.tf
@@ -0,0 +1,9 @@
+module "new-vpc" {
+  source="../../module/vpc"
+  vpc_cidr="${var.vpc_cidr}"
+  vpc_enable_dns="${var.vpc_enable_dns}"
+  for_each = var.subnet_cidr
+  subnet_cidr=each.value#"${var.subnet_cidr}"
+  availability_zone="${var.availability_zone}"
+}
+
