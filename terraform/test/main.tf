75c07aa7da6c7a8966aa386bca0ccfffe099d77c
Neh Lata <neh.lata@cloudeq.com>
2022-06-28 18:42:01 +0530

    check vpc

--- terraform/test/main.tf
+++
@@ -1,7 +0,0 @@
-module "new-bucket" {
-    source = "../"
-    bucket_name="new-bucket-instance"
-    name="neha-bucket"
-    Environment="dev"
-    acl="private"
-}
--- terraform/test/output.tf
+++
@@ -1,7 +0,0 @@
-
-output "name" {
-  value = "${module.new-bucket.bucket_id}"
-}
-output "ac" {
-  value = "${module.new-bucket.acl}"
-}
